04cb80916f43c540b9d5b277b09c90268f2511ca
h0llyp0llu <dark.holy.wend@gmail.com>
2022-06-23 01:17:11 +0700

    initialize react js typescript and tailwindcss added hero section for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
-function App() {
+export default function Example() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-white">
+      <header>
+        <section id="hero" className="relative bg-white">
+          <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <span className="text-3xl text-blue-600 font-bold">
+                Cinta Coding
+              </span>
+            </div>
+            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+              <a
+                href="#"
+                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Sign in
+              </a>
+            </div>
+          </div>
+        </section>
       </header>
+
+      <main>
+        <div>
+          {/* Hero card */}
+          <div className="relative">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+              <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+                <div className="absolute inset-0">
+                  <img
+                    className="h-full w-full object-cover"
+                    src="/images/arnold-francisca-f77Bh3inUpE-unsplash.jpg"
+                    alt="People working on laptops"
+                  />
+                  <div className="absolute inset-0 bg-indigo-700 mix-blend-multiply" />
+                </div>
+                <div className="relative px-4 py-16 sm:px-8 sm:py-36 lg:py-48 lg:px-10">
+                  <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                    <span className="block text-white">One Code Solution</span>
+                    <span className="block text-indigo-200">Test Case</span>
+                  </h1>
+                  <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
+                    Sebagai salah satu rangkaian test untuk bergabung dengan One
+                    Code Solution.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* More main page content here... */}
+      </main>
     </div>
   );
 }
-
-export default App;
